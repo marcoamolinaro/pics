5a20b81c423747d8d44ccd9c331f73ef92dc8e18
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2019-10-18 18:11:59 -0300

    93. Binding Callbacks

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import SearchBar from './SearchBar';
 class App extends React.Component {
     state = {images: []};
 
-    async onSearchSubmit(term) {
+    onSearchSubmit = async term => {
         const response = await axios.get('https://api.unsplash.com/search/photos', {
             headers: {
                 Authorization: 'Client-ID 53de0c5c1817c770f156302503f78a79b8951424a065aba7d0c9337a3bf103f4'
